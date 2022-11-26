fe60d5a25d91808bdb0ebd1231c0d5bc74844bdf
rion <riiiiion@gmail.com>
2022-11-26 22:50:34 +0900

    デプロイ頑張ってます

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,36 @@
-// Update with your config settings.
-// ddddd
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-//   client: 'postgresql',
-//     connection: {
-//       database: process.env.POSTGRES_DB || "music_diary" ,
-//       user: process.env.POSTGRES_USER || "postgres" ,
-//       password: process.env.PASSWORD|| null
-//     },
-//     migrations: {
-//       directory: "./db/migrations",
-//     },
-//     seeds: {
-//       directory: "./db/seeds",
-//     },
-
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.POSTGRES_DB || "music_diary",
+      user: process.env.POSTGRES_USER || "postgres",
+      password: process.env.PASSWORD || null,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: { directory: "./db/seeds" },
+  },
+};
 // }
 // client: "pg",
 //     connection: {
@@ -32,27 +42,15 @@ module.exports = {
 //       directory: "./db/migrations",
 //     },
 //     seeds: { directory: "./db/seeds" },
-  // },
-  client: "pg",
-    connection:process.env.DATABASE_URL,
-    ssl: {
-          rejectUnauthorized: false,
-        },
-        migrations: {
-          directory: "./db/migrations",
-        },
-        seeds: { directory: "./db/seeds" },
-      
-  }
-  // production: {
-  //   client: "pg",
-  //   connection: process.env.DATABASE_URL,
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
-  //   migrations: {
-  //     directory: "./db/migrations",
-  //   },
-  //   seeds: { directory: "./db/seeds" },
-  // },
-// };
+// },
+// client: "pg",
+//   connection:process.env.DATABASE_URL,
+//   ssl: {
+//         rejectUnauthorized: false,
+//       },
+//       migrations: {
+//         directory: "./db/migrations",
+//       },
+//       seeds: { directory: "./db/seeds" },
+
+// }
